2b54ee68b24405396fe956621ad5b4cedaa244e3
Kunal Wagle <kmw13@ic.ac.uk>
2019-07-04 18:05:37 +0100

    Removing library tests

--- src/test/java/codingchallenge/api/GitTest.java
+++ src/test/java/codingchallenge/api/GitTest.java
@@ -1,44 +1,44 @@
-package codingchallenge.api;
-
-import codingchallenge.api.implementations.GitImpl;
-import codingchallenge.domain.GitRepo;
-import codingchallenge.domain.subdomain.Language;
-import codingchallenge.services.interfaces.FileService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.kohsuke.github.GHCreateRepositoryBuilder;
-import org.kohsuke.github.GitHub;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.IOException;
-
-import static org.mockito.Mockito.*;
-
-@RunWith(MockitoJUnitRunner.class)
-@SpringBootTest
-public class GitTest {
-
-    @Mock
-    private FileService fileService;
-
-    @Mock
-    private GitHub gitHub;
-
-    @InjectMocks
-    private GitImpl git;
-
-    @Test
-    public void shouldCreateRepository() throws IOException {
-        GitRepo repo = new GitRepo("username", Language.JAVA, 1);
-        repo.setRepoName("repoName");
-//        when(gitHub.createRepository("repoName")).thenReturn();
-
-        git.createRepository(repo);
-
-
-    }
-}
+//package codingchallenge.api;
+//
+//import codingchallenge.api.implementations.GitImpl;
+//import codingchallenge.domain.GitRepo;
+//import codingchallenge.domain.subdomain.Language;
+//import codingchallenge.services.interfaces.FileService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.kohsuke.github.GHCreateRepositoryBuilder;
+//import org.kohsuke.github.GitHub;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.Mockito;
+//import org.mockito.junit.MockitoJUnitRunner;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.io.IOException;
+//
+//import static org.mockito.Mockito.*;
+//
+//@RunWith(MockitoJUnitRunner.class)
+//@SpringBootTest
+//public class GitTest {
+//
+//    @Mock
+//    private FileService fileService;
+//
+//    @Mock
+//    private GitHub gitHub;
+//
+//    @InjectMocks
+//    private GitImpl git;
+//
+//    @Test
+//    public void shouldCreateRepository() throws IOException {
+//        GitRepo repo = new GitRepo("username", Language.JAVA, 1);
+//        repo.setRepoName("repoName");
+////        when(gitHub.createRepository("repoName")).thenReturn();
+//
+//        git.createRepository(repo);
+//
+//
+//    }
+//}
